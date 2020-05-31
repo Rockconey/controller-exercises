f228ad3776623ec6dc423fb5ffe9c2813bb2e6d5
Shaw Coney <rockconey@gmail.com>
2020-05-31 15:05:31 -0500

    thymeleaf templates added

--- hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,11 +1,14 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 
 @Controller
-@ResponseBody
+//@ResponseBody
 @RequestMapping("hello")
 public class HelloController {
     //Tells it that it to handle path at /hello
@@ -16,6 +19,7 @@ public class HelloController {
 
     //lives at /hello/goodbye
     @GetMapping("goodbye")
+    @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
@@ -23,28 +27,27 @@ public class HelloController {
     //Handles requests of the form /hello?name=LaunchCode
     @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
 //    @GetMapping("hello")
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String helloWithQueryParam(@RequestParam String name, Model model) {
+        String aGreeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", aGreeting); //aGreeting is the attribute value. The attribute name is specified in the template.
+        return "hello";
     }
 
     //Responds to get requests at /hello/LaunchCode
     @GetMapping("hello/{name}")
-    public String helloAgain(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    public String helloAgain(@PathVariable String name, Model model) {
+        model.addAttribute("greeting", "Hello, " + name + "!");
+        return "hello";
     }
 
     @GetMapping("form")
+//    @ResponseBody
     public String helloForm() {
-        return "<html>" +
-        "<body>" +
-        "<form method='post' action='hello'>" + //Default method is get. So you can do it that way.
-        "<input type='text' name='name'>" +
-        "<input type='submit' value='Greet Me!'>" +
-        "</form>" +
-        "</body>" +
-        "</html>";
+            return "form";
     }
+
     @GetMapping("language")
+    @ResponseBody
     public String helloLanguage() {
         return "<html>" +
                     "<body>" +
@@ -66,6 +69,7 @@ public class HelloController {
         }
 
     @RequestMapping(value = "world", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
     public static String createMessage(@RequestParam String name, @RequestParam String language) {
         switch (language) {
             case "french":
@@ -78,6 +82,16 @@ public class HelloController {
                 return "Mholo, " + name + "!";
         }
        return "Hello, " + name + "!";
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        model.addAttribute("names", names);
+        return "hello-list";
     }
 
 
